386b65e15a78ba28f1489c617f6f173f82ed6076
hextrov <hextrov@bk.ru>
2015-03-31 13:30:09 +0100

    toggle cc vavlues

--- remap.pl
+++ remap.pl
@@ -6,18 +6,37 @@ use Getopt::Long;
 MIDI::ALSA::client('midi_remap', 1, 1, 0);
 
 my $help = '';
+my $toggle = '';
 
-GetOptions ("help" => \$help)
+GetOptions ("help" => \$help,
+            "toggle" => \$toggle)
         or help();
 
     if ($help == 1) { help(); }
+
+my @ccs = (0) x 127;
+my $ccv = 0;
 
 while(1) {
     my @alsaevent = MIDI::ALSA::input();
     if ($alsaevent[0] == SND_SEQ_EVENT_PGMCHANGE()) {
         my $ch = $alsaevent[7][0];
         my $pc = $alsaevent[7][5];
-        my @msg = MIDI::ALSA::controllerevent($ch, $pc, 0);
+       
+
+        # toggle cc values, effects on/off etc
+    
+        if ($toggle == 1) {
+            if ($ccs[$pc] == 0) {
+                $ccs[$pc] = 127;
+                $ccv = 127;
+            } else {
+                $ccs[$pc] = 0;
+                $ccv = 0;
+            };
+        };
+
+        my @msg = MIDI::ALSA::controllerevent($ch, $pc, $ccv);
         MIDI::ALSA::output(@msg);
     }
 }
@@ -25,5 +44,6 @@ while(1) {
 sub help
 {
     print("-h --help            This help \n");
+    print("-t --toggle          Toggle CC values \n");
     exit;
 }
